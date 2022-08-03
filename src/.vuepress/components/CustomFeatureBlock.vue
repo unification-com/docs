c4546b61b553d53f92666db338c4fb83f7f956fa
Codegnosis <paul@codegnosis.co.uk>
2022-08-03 11:40:17 +0100

    custom features block

---
+++ src/.vuepress/components/CustomFeatureBlock.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="features">
+    <div class="feature">
+      <h2>Mainchain</h2>
+      <p>
+        Documentation for all things related to Mainchain and the FUND network, indcluding:
+      </p>
+      <ul>
+        <li>
+          Installing und
+        </li>
+        <li>
+          Running a Validator node
+        </li>
+        <li>
+          Interaction & Txs
+        </li>
+      </ul>
+      <p class="action">
+        <a href="/mainchain/" class="nav-link action-button">
+          Go →
+        </a>
+      </p>
+    </div>
+    <div class="feature"><h2>OoO</h2>
+      <p>Documentation for OoO, including:</p>
+      <ul>
+        <li>
+          Deploying Contracts to use OoO
+        </li>
+        <li>
+          Interacting with the OoO API
+        </li>
+      </ul>
+      <p class="action">
+        <a href="/ooo/" class="nav-link action-button">
+          Go →
+        </a>
+      </p>
+    </div>
+    <div class="feature"><h2>VOR</h2>
+      <p>VOR Documentation, including:</p>
+      <ul>
+        <li>
+          Deploying Contracts to use VOR
+        </li>
+        <li>
+          Requesting VOR data
+        </li>
+      </ul>
+      <p class="action">
+        <a href="/vor/" class="nav-link action-button">
+          Go →
+        </a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    features: { required: true, type: Array },
+  }
+}
+</script>
