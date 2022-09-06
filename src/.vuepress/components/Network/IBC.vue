f6b72b9d5b6f6b9d2d9855a43ca0af7d58894df6
Codegnosis <paul@codegnosis.co.uk>
2022-09-06 13:31:59 +0100

    mainchain - add IBC relayers to network overview

---
+++ src/.vuepress/components/Network/IBC.vue
@@ -0,0 +1,41 @@
+<template>
+  <table>
+    <table v-for="item in relayers">
+      <thead>
+      <th></th>
+      <th>Source</th>
+      <th>Destination</th>
+      </thead>
+      <tbody>
+      <tr>
+        <td>Name</td>
+        <td>{{ item.src.chain_name}}</td>
+        <td>{{ item.dest.chain_name}}</td>
+      </tr>
+      <tr>
+        <td>Chain ID</td>
+        <td>{{ item.src.chain_id}}</td>
+        <td>{{ item.dest.chain_id}}</td>
+      </tr>
+      <tr>
+        <td>Channel</td>
+        <td>{{ item.src.channel}}</td>
+        <td>{{ item.dest.channel}}</td>
+      </tr>
+      <tr>
+        <td>Coin</td>
+        <td>{{ item.src.coin_id}}</td>
+        <td>{{ item.dest.coin_id}}</td>
+      </tr>
+      </tbody>
+    </table>
+  </table>
+</template>
+
+<script>
+export default {
+  props: {
+    relayers: { required: true },
+  }
+}
+</script>
